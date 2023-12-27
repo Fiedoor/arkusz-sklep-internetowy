9bb21973e961f4b7f6e88feea0a8bfb749798fe1
Fiedor <stachfie@wp.pl>
2023-12-27 11:54:15 +0100

    Created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sklep papierniczy</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
